742270f37603bcb4b7e0c302f855c068526ba81d
Rasa Tizdast <rasaknifehand@gmail.com>
2026-08-10 20:03:36 +0330

    chore(next): enable standalone output for container deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
+  turbopack: {
+    root: path.join(process.cwd()),
+  },
 };
 
 export default nextConfig;
